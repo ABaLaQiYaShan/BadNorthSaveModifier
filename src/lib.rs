a7402c1032b220a06c43bd55fb968888b03ccdfd
ShanMai <3407721412@qq.com>
2026-05-07 09:30:46 +0800

    删除“人数修改”和旧结构相关的空壳文件

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub mod models;
 pub mod save_manager;
-pub mod hero_editor;
 pub mod ui;
 
 pub use models::*;
--- src/ui/components/mod.rs
+++ src/ui/components/mod.rs
@@ -1,6 +1,2 @@
 
-pub mod file_selector;
-pub mod hero_list;
-pub mod hero_editor;
-pub mod toolbar;
 
